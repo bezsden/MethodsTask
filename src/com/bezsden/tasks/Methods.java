52a415657ee221e1184e89d0b0ea014ccf330f00
bezsden <bess.lex@gmail.com>
2018-08-21 12:13:32 +0300

    methods separated from run

---
+++ src/com/bezsden/tasks/Methods.java
@@ -0,0 +1,22 @@
+package com.bezsden.tasks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Methods {
+    public Methods() {
+    }
+
+    void initRun() {
+        List list = new ArrayList();
+        list.add("a1");
+        list.add("a4");
+        list.add("a3");
+        list.add("a2");
+        list.add("a1");
+        list.add("a4");
+    }
+    static void removeDuplicates(){
+
+    }
+}
--- src/com/bezsden/tasks/RunBoyRun.java
+++ src/com/bezsden/tasks/RunBoyRun.java
@@ -1,4 +1,19 @@
 package com.bezsden.tasks;
 
+import java.util.*;
+
 public class RunBoyRun {
+     Methods instanceOfObject = new Methods();
+
+     public static void main(String[] args) {
+        instanceOfObject.initRun();
+       // Collections.sort(list);
+        //System.out.println(list.toString());
+
+
+    }
+    
+
+
+
 }
